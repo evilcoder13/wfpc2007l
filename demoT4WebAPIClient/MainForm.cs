813dc8833092dbf95d1f5abb7d2343786367e389
Dao Manh Thang <xatm@mail15.com>
2022-02-18 20:52:16 +0700

    Demo ngay 2022-02-18

---
+++ demoT4WebAPIClient/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoT4WebAPIClient
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void aPiFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
